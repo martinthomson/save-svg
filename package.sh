c179f9dd18aa85f907818029922802c51bd08b08
Martin Thomson <mt@lowentropy.net>
2023-11-24 17:52:13 +1100

    Update package

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 v="$(jq -r .version < manifest.json)"
-zip "save-svg-$v.zip" manifest.json background.js
+zip "save-svg-$v.zip" manifest.json background.js icon.svg
 git tag "v$v" -am "Version $v release"
