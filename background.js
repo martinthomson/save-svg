dd376c223f4dac588ead0ca7bdcce254e16d46b0
Martin Thomson <mt@lowentropy.net>
2023-11-27 13:14:16 +1100

    Unconditionally register listeners

--- background.js
+++ background.js
@@ -12,32 +12,25 @@ function save_svg(id) {
   a.click();
 }
 
-function listen() {
-  browser.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId !== MENU) {
-      return;
-    }
-    try {
-      await browser.scripting.executeScript({
-        target: { tabId: tab.id, frameIds: [ info.frameId ] },
-        args: [info.targetElementId],
-        func: save_svg,
-      })
-    } catch(e) {
-      console.warn(`Error saving SVG: ${e}`);
-    }
-  });
-}
-
-function install() {
+browser.runtime.onInstalled.addListener(() => {
   browser.contextMenus.create({
     id: MENU,
     title: "Save SVG...",
     contexts: ["page", "frame", "link", "selection"],
   }, () => void browser.runtime.lastError);
+});
 
-  listen();
-}
-
-browser.runtime.onInstalled.addListener(install);
-browser.runtime.onStartup.addListener(listen);
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId !== MENU) {
+    return;
+  }
+  try {
+    await browser.scripting.executeScript({
+      target: { tabId: tab.id, frameIds: [ info.frameId ] },
+      args: [info.targetElementId],
+      func: save_svg,
+    })
+  } catch(e) {
+    console.warn(`Error saving SVG: ${e}`);
+  }
+});
